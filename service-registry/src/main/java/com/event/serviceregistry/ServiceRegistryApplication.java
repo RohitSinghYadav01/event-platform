6d9597a2fdd58c9f0dca24dc48e5abf230c8793a
Rohit Singh Yadav <rohit.singh.yadav.100@gmail.com>
2026-02-21 20:27:03 +0530

    Added User Service

--- service-registry/src/main/java/com/event/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/event/serviceregistry/ServiceRegistryApplication.java
@@ -3,11 +3,10 @@ package com.event.serviceregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-c
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
